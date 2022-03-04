c4dd3b301da2f69e4d044bcb337d6187cb06f286
ShilpiDas <shilpi12@in.ibm.com>
2022-03-04 12:55:40 +0530

    Adding OVN job

--- vars/setupCommonEnvironmentVariables.groovy
+++ vars/setupCommonEnvironmentVariables.groovy
@@ -105,6 +105,7 @@ def call() {
             env.CHRONY_CONFIG="true"
             env.SCG_ID = "c5045866-7a78-4b5b-8ae8-061340be64f8"
             env.VOLUME_STORAGE_TEMPLATE = "c340f1_v7k base template"
+            env.CNI_NETWORK_PROVIDER = "OpenshiftSDN"
 
             //e2e variables
             if ( env.ENABLE_E2E_TEST ) {
