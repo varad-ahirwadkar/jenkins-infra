2c509ab65c073f846716be62b0f15ec7d15c7c1b
SUDEESH JOHN <sudeeshjohn@in.ibm.com>
2022-10-12 16:27:07 +0530

    Formatting slack messages

--- vars/odfProcessTierTestResults.groovy
+++ vars/odfProcessTierTestResults.groovy
@@ -7,10 +7,10 @@ def call() {
             slacksummary = "Tier test didn't run"
         }
         if ( env.FAILED_STAGE != ""  ) {
-            env.MESSAGE = "ODF version: `${env.ODF_VERSION}` , ODF build: `${env.ODF_BUILD}`,  Tier: `${env.TIER_TEST}` , Failed Stage: `${env.FAILED_STAGE}` "
+            env.MESSAGE = "ODF version: `${env.ODF_VERSION}` , ODF build: `${env.ODF_BUILD}` ,  Tier: `${env.TIER_TEST}` , Failed Stage: `${env.FAILED_STAGE}` "
         }
         else {
-            env.MESSAGE = "ODF version: `${env.ODF_VERSION}` , ODF build: `${env.ODF_BUILD}` , Tier: `${env.TIER_TEST}` , Tier test summary:`${slacksummary}`"
+            env.MESSAGE = "ODF version: `${env.ODF_VERSION}` , ODF build: `${env.ODF_BUILD}` , Tier: `${env.TIER_TEST}` , Tier test summary: `${slacksummary}` "
         }
     }
 }
