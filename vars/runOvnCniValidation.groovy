945e94c32713f3aa347e79e4fde96e1cde9f0f92
ShilpiDas <shilpi12@in.ibm.com>
2022-10-20 11:30:46 +0530

    Changing E2e to run from ocp4-playbooks-extras

--- vars/runOvnCniValidation.groovy
+++ vars/runOvnCniValidation.groovy
@@ -9,22 +9,26 @@ def call(){
                 rm -rf ~/.ansible
                 ansible all -m setup -a 'gather_subset=!all'
                 cd ${WORKSPACE}/ocp4-playbooks-extras
-                cp examples/ocp_ovnkube_cni_vars.yaml vars.yaml
-                sed -i "s|ocp_ovnkube_cni:.*$|ocp_ovnkube_cni: true|g" vars.yaml
-                sed -i "s|ovnkube_cni_tests_enabled:.*$|ovnkube_cni_tests_enabled: true|g" vars.yaml
-                sed -i "s|egressfirewall_tests_enabled:.*$|egressfirewall_tests_enabled: true|g" vars.yaml
-                sed -i "s|egressip_tests_enabled:.*$|egressip_tests_enabled: true|g" vars.yaml
-                sed -i "s|networkpolicy_tests_enabled:.*$|networkpolicy_tests_enabled: true|g" vars.yaml
-                cat vars.yaml
-                cp examples/inventory .
-                sed -i "s|localhost|${BASTION_IP}|g" inventory
-                sed -i 's/ansible_connection=local/ansible_connection=ssh/g' inventory
-                sed -i "s|ssh|ssh ansible_ssh_private_key_file=${WORKSPACE}/deploy/id_rsa|g" inventory
-                cat inventory
+                cp examples/ocp_ovnkube_cni_vars.yaml ocp_ovnkube_cni_vars.yaml
+                sed -i "s|ocp_ovnkube_cni:.*$|ocp_ovnkube_cni: true|g" ocp_ovnkube_cni_vars.yaml
+                sed -i "s|ovnkube_cni_tests_enabled:.*$|ovnkube_cni_tests_enabled: true|g" ocp_ovnkube_cni_vars.yaml
+                sed -i "s|egressfirewall_tests_enabled:.*$|egressfirewall_tests_enabled: true|g" ocp_ovnkube_cni_vars.yaml
+                sed -i "s|egressip_tests_enabled:.*$|egressip_tests_enabled: true|g" ocp_ovnkube_cni_vars.yaml
+                sed -i "s|networkpolicy_tests_enabled:.*$|networkpolicy_tests_enabled: true|g" ocp_ovnkube_cni_vars.yaml
+                cat ocp_ovnkube_cni_vars.yaml
+                cp examples/inventory ./cni_inventory
+                sed -i "s|localhost|${BASTION_IP}|g" cni_inventory
+                sed -i 's/ansible_connection=local/ansible_connection=ssh/g' cni_inventory
+                sed -i "s|ssh|ssh ansible_ssh_private_key_file=${WORKSPACE}/deploy/id_rsa|g" cni_inventory
+                cat cni_inventory
+                cp ansible.cfg ansible_copy.cfg
                 echo "[ssh_connection]" >> ansible.cfg
                 echo "ssh_args = -C -o ControlMaster=auto -o ControlPersist=120m -o ServerAliveInterval=30" >> ansible.cfg
                 cat ansible.cfg
-                ansible-playbook  -i inventory -e @vars.yaml playbooks/ocp-ovnkube-cni.yml
+                ansible-playbook  -i cni_inventory -e @ocp_ovnkube_cni_vars.yaml playbooks/ocp-ovnkube-cni.yml
+                rm -f ansible.cfg
+                mv ansible_copy.cfg ansible.cfg
+                rm -f ansible_copy.cfg
             '''
         }
         catch (err) {
--- vars/setupAndRunE2e.groovy
+++ vars/setupAndRunE2e.groovy
@@ -8,24 +8,28 @@ def call(){
                 echo 'Creating var.yaml'
                 rm -rf ~/.ansible
                 ansible all -m setup -a 'gather_subset=!all'
-                cd ${WORKSPACE}/ansible_extra
-                cp examples/e2e_vars.yaml vars.yaml
-                sed -i "s|e2e_tests_enabled:.*$|e2e_tests_enabled: true|g" vars.yaml
-                sed -i "s|e2e_tests_git:.*$|e2e_tests_git: ${E2E_GIT}|g" vars.yaml
-                sed -i "s|e2e_tests_git_branch:.*$|e2e_tests_git_branch: ${E2E_BRANCH}|g" vars.yaml
-                sed -i "s|e2e_tests_exclude_list_url:.*$|e2e_tests_exclude_list_url: ${E2E_EXCLUDE_LIST}|g" vars.yaml
-                sed -i "s|golang_tarball:.*$|golang_tarball: ${GOLANG_TARBALL}|g" vars.yaml
-                sed -i "s|github_token:.*$|github_token: ${GITHUB_TOKEN}|g" vars.yaml
-                cat vars.yaml
-                cp examples/inventory .
-                sed -i "s|localhost|${BASTION_IP}|g" inventory
-                sed -i 's/ansible_connection=local/ansible_connection=ssh/g' inventory
-                sed -i "s|ssh|ssh ansible_ssh_private_key_file=${WORKSPACE}/deploy/id_rsa|g" inventory
-                cat inventory
+                cd ${WORKSPACE}/ocp4-playbooks-extras
+                cp examples/e2e_vars.yaml e2e_vars.yaml
+                sed -i "s|e2e_tests_enabled:.*$|e2e_tests_enabled: true|g" e2e_vars.yaml
+                sed -i "s|e2e_tests_git:.*$|e2e_tests_git: ${E2E_GIT}|g" e2e_vars.yaml
+                sed -i "s|e2e_tests_git_branch:.*$|e2e_tests_git_branch: ${E2E_BRANCH}|g" e2e_vars.yaml
+                sed -i "s|e2e_tests_exclude_list_url:.*$|e2e_tests_exclude_list_url: ${E2E_EXCLUDE_LIST}|g" e2e_vars.yaml
+                sed -i "s|golang_tarball:.*$|golang_tarball: ${GOLANG_TARBALL}|g" e2e_vars.yaml
+                sed -i "s|github_token:.*$|github_token: ${GITHUB_TOKEN}|g" e2e_vars.yaml
+                cat e2e_vars.yaml
+                cp examples/inventory ./e2e_inventory
+                sed -i "s|localhost|${BASTION_IP}|g" e2e_inventory
+                sed -i 's/ansible_connection=local/ansible_connection=ssh/g' e2e_inventory
+                sed -i "s|ssh|ssh ansible_ssh_private_key_file=${WORKSPACE}/deploy/id_rsa|g" e2e_inventory
+                cat e2e_inventory
+                cp ansible.cfg ansible_copy.cfg
                 echo "[ssh_connection]" >> ansible.cfg
                 echo "ssh_args = -C -o ControlMaster=auto -o ControlPersist=120m -o ServerAliveInterval=30" >> ansible.cfg
                 cat ansible.cfg
-                ansible-playbook  -i inventory -e @vars.yaml playbooks/main.yml
+                ansible-playbook  -i e2e_inventory -e @e2e_vars.yaml playbooks/ocp-e2e.yml
+                rm -f ansible.cfg
+                mv ansible_copy.cfg ansible.cfg
+                rm -f ansible_copy.cfg
             '''
         }
         catch (err) {
