e2aae06518e8d5dfec97a62ef6abe7baeba3f937
Neha Yadav <neha.yadav3@ibm.com>
2023-02-15 11:23:21 +0530

    Revert "change ocs-upi-kvm repo for testing"
    This reverts commit fbd289d94d87731bc32e2a454093c45cb8e80067.

--- vars/odfRunSetupScript.groovy
+++ vars/odfRunSetupScript.groovy
@@ -23,7 +23,7 @@ def call(){
                [ ! -z "$OCS_REGISTRY_IMAGE" ] && echo "export OCS_REGISTRY_IMAGE=${OCS_REGISTRY_IMAGE}" >> env_vars.sh
                [ ! -z "$RERUN_TIER_TEST" ] && echo "export RERUN_TIER_TEST=${RERUN_TIER_TEST}" >> env_vars.sh
                scp -i ${WORKSPACE}/deploy/id_rsa -o 'StrictHostKeyChecking=no' env_vars.sh root@${BASTION_IP}:/root/
-               ssh -o 'StrictHostKeyChecking=no' -i ${WORKSPACE}/deploy/id_rsa root@${BASTION_IP} "git clone https://github.com/Neha-dot-Yadav/ocs-upi-kvm.git"
+               ssh -o 'StrictHostKeyChecking=no' -i ${WORKSPACE}/deploy/id_rsa root@${BASTION_IP} "git clone https://github.com/ocp-power-automation/ocs-upi-kvm.git"
                ssh -o 'StrictHostKeyChecking=no' -i ${WORKSPACE}/deploy/id_rsa root@${BASTION_IP} "cd /root/ocs-upi-kvm; git submodule update --init;"
                ssh -o 'StrictHostKeyChecking=no' -i ${WORKSPACE}/deploy/id_rsa root@${BASTION_IP} "cp /root/openstack-upi/metadata.json /root/;"
                ssh -o 'StrictHostKeyChecking=no' -o 'ServerAliveInterval=5' -o 'ServerAliveCountMax=1200' -i ${WORKSPACE}/deploy/id_rsa root@${BASTION_IP} 'chmod 0755 env_vars.sh; source env_vars.sh; cd /root/ocs-upi-kvm/scripts/helper; /bin/bash ./kustomize.sh > kustomize.log'
